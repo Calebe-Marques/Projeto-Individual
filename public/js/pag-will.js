481af233985a41be9a3f3fc8474c4d49ee586497
Calebe-Marques <calebe.reboucas@sptech.school>
2022-11-27 12:42:14 -0300

    pag will finalizada

---
+++ public/js/pag-will.js
@@ -0,0 +1,45 @@
+const menu = document.querySelector('.nav-header')
+
+function ativarScroll() {
+    menu.classList.toggle('ativo', scrollY > 250);
+}
+
+window.addEventListener('scroll', ativarScroll);
+
+/* Parte carrossel */
+const controls = document.querySelectorAll(".control");
+
+let itemAtual = 0;
+
+const items = document.querySelectorAll(".item");
+
+const maxItems = items.length;
+
+controls.forEach(control =>{
+    control.addEventListener("click", () =>{
+        const isleft = control.classList.contains("arrow-left")
+        
+        if(isleft){
+            itemAtual -= 1;
+        }else{
+            itemAtual += 1
+        }
+
+        if(itemAtual >= maxItems){
+            itemAtual = 0;
+        }
+
+        if(itemAtual < 0){
+            itemAtual = maxItems - 1;
+        }
+
+        items.forEach((item) => item.classList.remove("item-atual"));
+
+        items[itemAtual].scrollIntoView({
+            inline: "center",
+            behavior: "smooth",
+            block: "nearest"
+        });
+        items[itemAtual].classList.add("item-atual");
+    });
+});
